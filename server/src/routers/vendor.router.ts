69860ffb13ede47c48460011f1f5589f474a4e3a
unknown <shahriar.khan099@gmail.com>
2024-01-14 17:18:27 +0600

    backend initially completed

--- server/src/routers/vendor.router.ts
+++ server/src/routers/vendor.router.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
-import { getAllVendors, getAllVendorsWithProducts, postVendor, updateVendor, removeVendor, getVendorByName, getVendorByProductName, getVendorById } from '../controllers/vendor.controller';
+import { getAllVendors, postVendor, updateVendor, removeVendor, getVendorByName, getVendorByProductName, getAllVendorsWithProducts, getVendorById } from '../controllers/vendor.controller';
 const router = Router();
 
 router.get('/', getAllVendors);
 router.post('/', postVendor);
 router.put('/:vendorId', updateVendor);
 router.delete('/:vendorId', removeVendor);
+router.get('/search', getVendorByName);
+
+router.get('/search/product', getVendorByProductName);
+router.get('/products', getAllVendorsWithProducts);
+router.get('/:vendorId', getVendorById);
 
 
 
